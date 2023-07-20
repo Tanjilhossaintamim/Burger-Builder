b991a4e15de3be564d8ef7de577ad67e4170999c
Tanjil Hossain <tanjilhossain845.com>
2023-07-20 20:36:19 +0600

    Added error boundary

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import "./App.css";
 import MainComponent from "./components/MainComponent";
 import ReduxStore from "./redux/Store";
 import { BrowserRouter } from "react-router-dom";
+import ErrorBoundary from "./components/Error/ErrorBoundary";
 function App() {
   return (
     <>
       <Provider store={ReduxStore}>
         <BrowserRouter>
-          <MainComponent />
+          <ErrorBoundary fallback={<h1>Something Went Wrong !</h1>}>
+            <MainComponent />
+          </ErrorBoundary>
         </BrowserRouter>
       </Provider>
     </>
